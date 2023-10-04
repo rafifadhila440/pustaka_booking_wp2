197d01a9bd1f90cf3c59d0aa81d53ea5099b4f3f
rafifadhila440 <rafifadhila440@gmail.com>
2023-10-04 20:43:48 +0700

    Contoh menampilkan biodata Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Rafi Fadhila Ramadhani saya tinggal di daerah Bogor Hobi saya adalah menjadi operator sound";
+    }
+}
+?>
